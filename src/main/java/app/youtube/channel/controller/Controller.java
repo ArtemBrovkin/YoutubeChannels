4af72bb64810e1299d1373b26430e05a1a3efab5
1t <rregregret@egfgfg.ru>
2022-11-30 15:45:16 +0300

    Controller: "/" -> "/channel"

--- src/main/java/app/youtube/channel/controller/Controller.java
+++ src/main/java/app/youtube/channel/controller/Controller.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/channel")
 @RequiredArgsConstructor
 public class Controller {
 
